e799f072a36b79a3857dd0f214267bdd6fb99730
MukhlisHasanov <muhlis.gasanov@gmail.com>
2024-06-09 01:56:29 +0400

    hw8

---
+++ lesson9/code/example_03/index.js
@@ -0,0 +1,48 @@
+// loops
+
+// const arr1 = [1, 4, 98, "abc", null, undefined, [1, 2, 3]];
+// arr1.length;
+
+// for (let i = 0; i < 5; i++ ) {
+//     console.log(i);
+// }
+
+
+// let i = 0;
+// while (i < 5) {
+//     console.log(i);
+//     i++;
+// }
+
+// // выводим элементы массива в консоль
+// for (let j = 0; j < 7; j++) {
+//     console.log(arr1[j]);  
+// }
+
+// создать пустой массив, заполнить случайными числами (Math.random()), 5 элементов (в цикле)
+const arrWithRandoms = [];
+
+for (let m = 0; m < 5; m++) {
+    let randomİnt = Math.floor(Math.random() * 100);
+    arrWithRandoms.push(randomİnt);
+}
+console.log(arrWithRandoms);
+
+// задачка про четверть часа
+
+// if
+// math
+// %
+// let minutes = 0 - 59;
+let minutes = 27;
+
+
+
+
+
+
+
+
+
+
+
