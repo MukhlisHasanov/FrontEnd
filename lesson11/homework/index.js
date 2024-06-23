5afc14ca7b9a8ca296e8a15c4302f95306ded666
MukhlisHasanov <muhlis.gasanov@gmail.com>
2024-06-24 01:52:31 +0400

    hw11-2

--- lesson11/homework/index.js
+++ lesson11/homework/index.js
@@ -1,5 +1,4 @@
 console.log('\n=====> task_01 <=====\n');
-
 /**
  * Напишите фукнцию, которая будет принимать массив. Массив может содержать любые строки и числа. 
  * Функция должна вернуть объект, который содержит свойства, ключ и значение которых равны соответствующему элементу массива. 
@@ -19,7 +18,6 @@ function arrayToObject(arr) {
 console.log(arrayToObject(examplArr));
 
 console.log('\n=====> task_02 <=====\n');
-
 /**
  * Напишите функцию, которая превратит массив который содержит элементы в виде массивов из двух элементов, в объект.
  * Первый элемент каждого внутреннего массива ключ, второй - значение. 
@@ -56,7 +54,6 @@ function makeNewObject(arrayWithDoubleElements) {
 console.log(makeNewObject(arrayWithDoubleElements));
 
 console.log('\n=====> task_03 <=====\n');
-
 /**
 * Функция должна принимать объект и возвращать строку, где будут перечислены все значения свойств объекта через запятую.
 * Пример работы функции:
@@ -72,7 +69,6 @@ function objToString(obj) {
 objToString(obj2);
 
 console.log('\n=====> task_04 <=====\n');
-
 /**
 * Напишите функцию, которая принимает объект и возвращает другой обьект,
 * который содержит все свойства исходного объекта,
@@ -90,9 +86,7 @@ function objToType(obj) {
 }
 objToType(obj2);
 
-
 console.log('\n=====> task_04* <=====\n');
-
 /**
  * 4*. Напишите функцию, которая принимает объект и возвращает другой более сложный обьект,
  * который содержит все свойства исходного обьетка, 
@@ -104,11 +98,82 @@ console.log('\n=====> task_04* <=====\n');
  * 'John Doe': { value: 'John Doe', type: 'string' } }
  */
 
-function objToType(obj) {
+function objToType2(obj) {
     const resultType = {};
     for (const key in obj) {
         resultType[key] = {value: obj[key], type: typeof(obj[key])};
     }
     return console.log(resultType);
 }
-objToType(obj2); 
+objToType2(obj2); 
+
+console.log('\n=====> task_05* <=====\n');
+/**
+ * 5*. Напишите функцию, которая посчитает сумму к оплате в корзине в интернет магазине комп техники. 
+ * На вход функция получает объект корзины (пример ниже - наименование товара и количество, а также обьект с ценами на товары, с доп. уровнями вложености). 
+ * К названиям товаров привязываться не стоит, они могут быть другими, в примере просто пример.
+ * const bucket = { display: 20, videocard: 100, cpu: 40, }
+ * const products = { warehouse1: { videocard: 12.5, }, warehouse2: { display: 56.2, cpu: 150, }, }
+ * calculateCost(bucket, products); // результат = 8374
+ */
+
+const bucket = { display: 20, videocard: 100, cpu: 40, };
+const products = { 
+    warehouse1: { videocard: 12.5, },
+    warehouse2: { display: 56.2, cpu: 150, }
+ };
+
+function calculateCost(bucket, products) {
+    let result = 0;
+    for (let key in bucket) {
+        const posOfShopCart = key;
+        const amountOfPos = bucket[key];
+        for (key in products) {
+            const warehouse = products[key];
+            for (key in warehouse) {
+                if (posOfShopCart === key) {
+                    result += warehouse[key] * amountOfPos;
+                }
+            }
+        }
+    }
+    return result;
+}
+console.log('Total price of shoppin cart:', calculateCost(bucket, products));
+
+console.log('\n=====> task_06* <=====\n');
+/**
+ * 6*. Напишите функцию, которая сравнивает 2 объекта, и возвращает результат true если они равны или false в противном случае
+ * Функция принимает 2 аргумента - объекты. Объекты в качестве значений свойств могут иметь только примитивные типы данных
+ * Объекты равные, если их содержание (пары ключ-значение) совпадает, количество свойств также должно совпадать. Порядок не важен.
+ * function compareObjects(firstObj, secondObj) { // тут ваш код }
+ * примеры объектов которые можно использовать для тестирования функции:
+ * const obj1 = { name: 'John', age: 17, };
+ * const obj2 = { name: 'Mark', age: 17, };
+ * const obj3 = { name: 'Mark', age: 17, student: false, };
+ * const obj4 = { name: 'John', age: 17, };
+*/
+
+const obj1 = { name: 'John', age: 17, };
+const obj22 = { name: 'Mark', age: 17, };
+const obj3 = { name: 'Mark', age: 17, student: false, };
+const obj4 = { name: 'John', age: 17, };
+
+function compareObjects(firstObj, secondObj) {
+    const keys1 = Object.keys(firstObj);
+    const keys2 = Object.keys(secondObj);
+    if (keys1.length !== keys2.length) {
+        return false;
+    }
+    for (let key in firstObj) {
+        if (!(key in secondObj) || firstObj[key] !== secondObj[key]) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log('compareObjects(obj1, obj2) is', compareObjects(obj1, obj22));    // false
+console.log('compareObjects(obj1, obj4) is', compareObjects(obj1, obj4));    // true
+console.log('compareObjects(obj2, obj3) is', compareObjects(obj22, obj3));    // false
+console.log('compareObjects(obj1, obj3) is', compareObjects(obj1, obj3));    // false
+console.log('compareObjects(obj3, obj3) is', compareObjects(obj3, obj3));    // true
