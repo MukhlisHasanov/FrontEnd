109c672dbd1e570522cb3bf895a2efd69a0344f1
MukhlisHasanov <muhlis.gasanov@gmail.com>
2024-06-24 00:46:33 +0400

    hw11

--- lesson11/homework/index.js
+++ lesson11/homework/index.js
@@ -21,7 +21,9 @@ console.log(arrayToObject(examplArr));
 console.log('\n=====> task_02 <=====\n');
 
 /**
- * Напишите функцию, которая превратит массив который содержит элементы в виде массивов из двух элементов, в объект. Первый элемент каждого внутреннего массива ключ, второй - значение. Например такой:
+ * Напишите функцию, которая превратит массив который содержит элементы в виде массивов из двух элементов, в объект.
+ * Первый элемент каждого внутреннего массива ключ, второй - значение. 
+ * Например такой:
 [
   [ 'height', 170 ],
   [ 'weight', 80 ],
@@ -52,3 +54,61 @@ function makeNewObject(arrayWithDoubleElements) {
 }
 
 console.log(makeNewObject(arrayWithDoubleElements));
+
+console.log('\n=====> task_03 <=====\n');
+
+/**
+* Функция должна принимать объект и возвращать строку, где будут перечислены все значения свойств объекта через запятую.
+* Пример работы функции:
+* { a: 'a', '36.6': 36.6, 'John Doe': 'John Doe' } ==> 'a, 36.6, John Doe'
+ */
+
+const obj2 = { a: 'a', '36.6': 36.6, 'John Doe': 'John Doe' };
+function objToString(obj) {
+    const valuesObj = Object.values(obj);
+    const resultStr = valuesObj.join(', ');
+    console.log(resultStr);
+}
+objToString(obj2);
+
+console.log('\n=====> task_04 <=====\n');
+
+/**
+* Напишите функцию, которая принимает объект и возвращает другой обьект,
+* который содержит все свойства исходного объекта,
+* но значения свойств - это их типы.
+* Пример:
+* { a: 'a', '36.6': 36.6, 'John Doe': 'John Doe' } ==> { a: 'string', '36.6': 'number', 'John Doe': 'string' }
+*/
+
+function objToType(obj) {
+    const resultType = {};
+    for (const [key, value] of Object.entries(obj)) {
+        resultType[key] = typeof value;
+    }
+    return console.log(resultType);
+}
+objToType(obj2);
+
+
+console.log('\n=====> task_04* <=====\n');
+
+/**
+ * 4*. Напишите функцию, которая принимает объект и возвращает другой более сложный обьект,
+ * который содержит все свойства исходного обьетка, 
+ * но значения свойств - это объект содержащий исходное значение, и его тип.
+ * Пример:
+ * { a: 'a', '36.6': 36.6, 'John Doe': 'John Doe' } ==> 
+ * { a: { value: 'a', type: 'string' }, 
+ * '36.6': { value: 36.6, type: 'number' }, 
+ * 'John Doe': { value: 'John Doe', type: 'string' } }
+ */
+
+function objToType(obj) {
+    const resultType = {};
+    for (const key in obj) {
+        resultType[key] = {value: obj[key], type: typeof(obj[key])};
+    }
+    return console.log(resultType);
+}
+objToType(obj2); 
