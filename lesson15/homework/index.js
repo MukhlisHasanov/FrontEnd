01756421352814b07f91837926581d77592c0080
MukhlisHasanov <muhlis.gasanov@gmail.com>
2024-06-28 20:44:11 +0400

    hw15-3

--- lesson15/homework/index.js
+++ lesson15/homework/index.js
@@ -6,7 +6,7 @@ console.log('\n=====> task_01 <=====\n');
 
 const names = ["Alex", "Boris", "Kirill", "Andrey", "Oleg", "Roman", "Arnold", "Leonid", "Eugene"];
 const namesFilterA = (arr) => {
-    return arr.filter(name => name[0].toLowerCase() === 'a');
+   return arr.filter(name => name[0].toLowerCase() === 'a');
 }
 console.log(namesFilterA(names));
 
@@ -17,44 +17,51 @@ console.log('\n=====> task_02 <=====\n');
  */
 
 const numbers = [];
-for(let i = 0; i < 10; i++) {
-numbers.push(Math.round(Math.random() * 10));
+for (let i = 0; i < 10; i++) {
+   numbers.push(Math.round(Math.random() * 10));
 }
 console.log(numbers);
 const sum = numbers.reduce((acc, num) => acc + num, 0);
 console.log('Sum of numbers is ' + sum);
-
-
 
 console.log('\n=====> task_03 <=====\n');
 
 /**
  * Написать функцию, которая принимает на вход массив чисел и возвращает массив из тех же элементов, но в обратном порядке.
  */
+const numbs = [];
+for (let i = 0; i < 10; i++) {
+   numbs.push(Math.round(Math.random() * 10));
+}
+console.log(numbs);
 
+const numbsVers = [];
+for (let j = numbs.length -1; j >= 0; j--) {
+   numbsVers.push(numbs[j]);
+}
+console.log(numbsVers);
 
 console.log('\n=====> task_04 <=====\n');
 /**
  *  Написать функцию, которая примет объект вида:
 */
 const customer = {
-    'customer-id-1': {
-       name: 'William',
-       age: 54
-    },
-    'customer-id-2': {
-       name: 'Tоm',
-       age: 17
-    }
- }
+   'customer-id-1': {
+      name: 'William',
+      age: 54
+   },
+   'customer-id-2': {
+      name: 'Tоm',
+      age: 17
+   }
+}
 
- const getObjectView = (customer) => {
-    const changeElement = (custId) => {
-        return {id: custId, ...customer[custId]}
-    }
-    const customerId = Object.keys(customer);
-    const newArray = customerId.map(changeElement);
-    return newArray;
- }
- console.log(getObjectView(customer));
- 
+const getObjectView = (customer) => {
+   const changeElement = (custId) => {
+      return { id: custId, ...customer[custId] }
+   }
+   const customerId = Object.keys(customer);
+   const newArray = customerId.map(changeElement);
+   return newArray;
+}
+console.log(getObjectView(customer));
